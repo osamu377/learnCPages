a5b447a43d0a7b5f36d33782183b5e733665e390
Osamu377 <osamu-ochi@outlook.com>
2026-01-04 19:10:59 +0900

    p2c1まで、少しブラッシュアップ

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-69ce5cea.js',
+                path_to_root + 'searchindex-e118c292.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
