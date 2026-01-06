2d85808d8ccca6acc24d3612a1905e53c8fb3b97
Osamu377 <osamu-ochi@outlook.com>
2026-01-06 21:54:45 +0900

    標準入出力のサンプルプログラム追加

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e118c292.js',
+                path_to_root + 'searchindex-d416fae2.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
