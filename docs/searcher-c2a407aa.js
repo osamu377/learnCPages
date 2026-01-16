b2cc970090dfdf9c97a71417c23c5b235d57d77f
Osamu377 <osamu-ochi@outlook.com>
2026-01-16 22:56:34 +0900

    再帰のアニメーションを追加

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-66240e1b.js',
+                path_to_root + 'searchindex-2da0a16e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
